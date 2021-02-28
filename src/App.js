3f5845a331b0e9716e5823f23bfa46a94a39e3ba
Anjelica Avorque <71037218+evanjelica@users.noreply.github.com>
2021-02-28 10:02:03 -0600

    pls push and work

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Services from './pages/Services/Services';
 import Products from './pages/Products/Products';
 import SignUp from './pages/SignUp/SignUp';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import ScrollToTop from './components/ScrollToTop';
 import { Navbar, Footer } from './components';
 
 function App() {
