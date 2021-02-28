63c46e11d2686db2bad90188d52e734c0b383b7b
Anjelica Avorque <71037218+evanjelica@users.noreply.github.com>
2021-02-28 10:13:07 -0600

    pls work pt 2

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import Home from './pages/HomePage/Home';
 import Services from './pages/Services/Services';
 import Products from './pages/Products/Products';
 import SignUp from './pages/SignUp/SignUp';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ScrollToTop from './components/ScrollToTop';
-import { Navbar, Footer } from './components';
 
 function App() {
   return (
