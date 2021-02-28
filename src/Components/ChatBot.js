54db42d789977c0b624e9ff3a24f8843a19bac5b
Anjelica Avorque <71037218+evanjelica@users.noreply.github.com>
2021-02-28 09:58:34 -0600

    Chatbox updates

--- src/Components/ChatBot.js
+++ src/Components/ChatBot.js
@@ -7,16 +7,11 @@ class ChatBot extends Component {
   componentDidMount() {
     (function(d, m){
       var kommunicateSettings = 
-          {"appId":"2bb7ad178df6773bff43ae751880c0373",
-          "popupWidget":true,
-          "automaticChatOpenOnNavigation":true};
-      var s = document.createElement("script");
-      s.type = "text/javascript";
-      s.async = true;
+          {"appId":"2bb7ad178df6773bff43ae751880c0373","popupWidget":true,"automaticChatOpenOnNavigation":true};
+      var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
       s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
       var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
-      window.kommunicate = m;
-      m._globals = kommunicateSettings;
+      window.kommunicate = m; m._globals = kommunicateSettings;
   })(document, window.kommunicate || {});
     /* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
   }
