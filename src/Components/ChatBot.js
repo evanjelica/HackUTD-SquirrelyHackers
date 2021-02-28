6b16bdc24f030c08a7ef937e5e83af04758564af
Anjelica Avorque <71037218+evanjelica@users.noreply.github.com>
2021-02-28 02:33:07 -0600

    panicc attack go brrr

---
+++ src/Components/ChatBot.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+class ChatBot extends Component {
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+    (function (d, m) {
+      var kommunicateSettings = {
+        appId: "cae188ceb7bd65a496627629d9e5cdff",
+        popupWidget: true,
+        automaticChatOpenOnNavigation: true,
+      };
+      var s = document.createElement("script");
+      s.type = "text/javascript";
+      s.async = true;
+      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+      var h = document.getElementsByTagName("head")[0];
+      h.appendChild(s);
+      window.kommunicate = m;
+      m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
+    /* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
+  }
+  render() {
+    return <div></div>;
+  }
+}
+
+export default ChatBot;
